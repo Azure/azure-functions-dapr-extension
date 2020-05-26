dd8966cd52b18031834c6a7d4c386596519cfd6c
Wenonah Zhang <wenhzha@microsoft.com>
2020-05-26 14:16:46 -0700

    also for test proj

--- test/DaprExtensionTests/Tests/DaprSecretBindingTests.cs
+++ test/DaprExtensionTests/Tests/DaprSecretBindingTests.cs
@@ -1,5 +1,7 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
 
 namespace DaprExtensionTests.Tests
 {
