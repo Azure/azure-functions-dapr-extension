dee9a98643e8c40736998df8e5747959c6bedd9d
Wenonah Zhang <wenhzha@microsoft.com>
2020-06-26 15:17:34 -0700

    remove unused namespace

--- src/Dapr.AzureFunctions.Extension/Bindings/DaprSecretConverter.cs
+++ src/Dapr.AzureFunctions.Extension/Bindings/DaprSecretConverter.cs
@@ -5,9 +5,7 @@
 
 namespace Dapr.AzureFunctions.Extension
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
     using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
