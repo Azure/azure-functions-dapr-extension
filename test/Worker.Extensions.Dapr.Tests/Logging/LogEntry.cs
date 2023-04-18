937b8a250e0e11f5a9a7870bf418093b05c2d714
MD Ashique <noorani.ashique5@gmail.com>
2023-04-18 12:01:36 +0530

    Added test project for isolated worker

---
+++ test/Worker.Extensions.Dapr.Tests/Logging/LogEntry.cs
@@ -0,0 +1,31 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+namespace DaprExtensionTests.Logging
+{
+    using System;
+    using Microsoft.Extensions.Logging;
+
+    class LogEntry
+    {
+        public LogEntry(LogLevel level, string message)
+        {
+            this.LogLevel = level;
+            this.Message = message;
+            this.Timestamp = DateTime.Now;
+        }
+
+        public DateTime Timestamp { get; }
+
+        public LogLevel LogLevel { get; }
+
+        public string Message { get; }
+
+        public override string ToString()
+        {
+            return $"{this.Timestamp:o}: {this.Message}";
+        }
+    }
+}
---
+++ test/Worker.Extensions.Dapr.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Worker.Extensions.Dapr.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
