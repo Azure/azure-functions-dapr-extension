12bba3d87a7bdce2bc9f12d9a0b1371d6be078ba
Shubham Sharma <shubhash@microsoft.com>
2023-04-28 12:02:17 +0530

    Add service invoke example python
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

---
+++ samples/python-azurefunction/InvokeOutputBinding/__init__.py
@@ -0,0 +1,26 @@
+import json
+import logging
+import azure.functions as func
+
+
+def main(req: func.HttpRequest,
+         payload: func.Out[bytes]) -> func.HttpResponse:
+    logging.info('Python InvokeOutputBinding processed a request.')
+    data = req.params.get('data')
+    if not data:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            data = req_body.get('data')
+
+    if data:
+        logging.info(f"Url: {req.url}, Data: {data}")
+        payload.set(json.dumps({"data": data}).encode('utf-8'))
+        return func.HttpResponse(f"Url: {req.url}, Data: {data}")
+    else:
+        return func.HttpResponse(
+            "Please pass a data on the query string or in the request body",
+            status_code=400
+        )
