d398bc87755bed81004beb7bfadcdf2e531db11a
MD Ashique <noorani.ashique5@gmail.com>
2023-05-04 17:11:42 +0530

    Added samples for jsonelement
    Signed-off-by: MD Ashique <noorani.ashique5@gmail.com>

--- samples/dotnet-isolated-azurefunction/OuputBinding/DaprStateOutputBindingJsonElementType.cs
+++ samples/dotnet-isolated-azurefunction/OuputBinding/DaprStateOutputBindingJsonElementType.cs
@@ -6,23 +6,24 @@
 namespace dotnet_isolated_azurefunction
 {
     using System.Text.Json;
+    using System.Text.Json.Serialization;
     using Microsoft.Azure.Functions.Worker;
     using Microsoft.Azure.Functions.Worker.Extensions.Dapr;
     using Microsoft.Extensions.Logging;
 
-    public static class DaprStateOutputBindingUserDefinedType
+    public static class DaprStateOutputBindingJsonElementType
     {
         /// <summary>
         /// Example to use Dapr Service Invocation Trigger and Dapr State Output binding to persist a new state into statestore
         /// </summary>
-        [Function("DaprStateOutputBindingUserDefinedType")]
-        [DaprStateOutput("%StateStoreName%", Key = "product")]
-        public static Product Run(
-            [DaprServiceInvocationTrigger] Product payload,
+        [Function("DaprStateOutputBindingJsonElementType")]
+        [DaprStateOutput("%StateStoreName%", Key = "DaprStateBindingJsonElementType")]
+        public static JsonElement Run(
+            [DaprServiceInvocationTrigger] JsonElement payload,
             FunctionContext functionContext)
         {
-            var log = functionContext.GetLogger("DaprStateOutputBindingUserDefinedType");
-            log.LogInformation("C# function processed a DaprStateOutputBindingUserDefinedType request from the Dapr Runtime.");
+            var log = functionContext.GetLogger("DaprStateOutputBindingJsonElementType");
+            log.LogInformation("C# function processed a DaprStateOutputBindingJsonElementType request from the Dapr Runtime.");
 
             return payload;
         }
