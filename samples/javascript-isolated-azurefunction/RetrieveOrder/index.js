91fcfa0fc0fb82937b7ed33312e1fb1c27f2dc9c
MD Ashique <noorani.ashique5@gmail.com>
2023-05-08 12:25:13 +0530

    Removing separate samples of javascript isolated worker, as its not required. js already runs in isolated mode
    Signed-off-by: MD Ashique <noorani.ashique5@gmail.com>

--- samples/javascript-isolated-azurefunction/RetrieveOrder/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function (context) {
-    context.log("Node function processed a RetrieveOrder request from the Dapr Runtime.");
-
-    // print the fetched state value
-    context.log(context.bindings.data);
-};
