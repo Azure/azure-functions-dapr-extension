065ebe09ea0bb7f100095268b9148d3697d34756
Shubham Sharma <shubhash@microsoft.com>
2023-05-16 12:04:27 +0530

    Update examples
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- samples/javascript-azurefunction/PublishOutputBinding/index.js
+++ samples/javascript-azurefunction/PublishOutputBinding/index.js
@@ -1,5 +1,4 @@
 module.exports = async function (context, req) {
     context.log("Node HTTP trigger function processed a request.");
     context.bindings.payload = { payload: req.body };
-    context.done(null);
 };
