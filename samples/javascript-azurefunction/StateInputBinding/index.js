381868210aa21bebed4954a94e32495a98c3fe2e
Shubham Sharma <shubhash@microsoft.com>
2023-05-16 12:33:03 +0530

    Fix state input/output
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- samples/javascript-azurefunction/StateInputBinding/index.js
+++ samples/javascript-azurefunction/StateInputBinding/index.js
@@ -1,18 +1,4 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    context.log('Current state of this function: ' + context.bindings.daprState);
-
-    context.log('Sending data to function 2 to continue processing');
-
-    context.bindings.daprInvoke = {
-        appId: 'function2',
-        methodName: 'process',
-        httpVerb: 'post',
-        body: context.bindings.daprState
-    }
-
-    context.res = {
-        status: 200
-    };
+    context.log('JavaScript HTTP trigger function processed StateInputBinding request.');
+    context.log('Current state of this function: ' + JSON.stringify(context.bindings.state));
 };
