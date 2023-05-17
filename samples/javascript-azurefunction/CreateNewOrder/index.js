39cb143914c945b140d588fb6f30d60463948106
Shubham Sharma <shubhash@microsoft.com>
2023-05-17 11:54:22 +0530

    Updates to JavaScript sample (#110)
    * Update examples
    
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>
    
    * Fix state input/output
    
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>
    
    * More changes;
    
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>
    
    * More changes;
    
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>
    
    ---------
    
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- samples/javascript-azurefunction/CreateNewOrder/index.js
+++ samples/javascript-azurefunction/CreateNewOrder/index.js
@@ -1,4 +1,4 @@
 module.exports = async function (context) {
     context.log("Node function processed a CreateNewOrder request from the Dapr Runtime.");
-    context.bindings.order = {"value": context.bindings.payload["data"]};
+    context.bindings.order = { "value": context.bindings.payload["data"] };
 };
