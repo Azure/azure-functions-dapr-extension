b96b284fc2d208586b0f660d3051190d6544230c
Shubham Sharma <shubhash@microsoft.com>
2023-05-17 18:35:51 +0530

    Changes to python samples
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- samples/python-azurefunction/PrintTopicMessage/__init__.py
+++ samples/python-azurefunction/PrintTopicMessage/__init__.py
@@ -1,8 +1,9 @@
 import logging
 import json
-import azure.functions as func
+
 
 def main(subEvent) -> None:
-    logging.info('Python function processed a PrintTopicMessage request from the Dapr Runtime.')
+    logging.info(
+        'Python function processed a PrintTopicMessage request from the Dapr Runtime.')
     subEvent_json = json.loads(subEvent)
     logging.info("Topic B received a message: " + subEvent_json["data"])
--- samples/python-azurefunction/TransferEventBetweenTopics/__init__.py
+++ samples/python-azurefunction/TransferEventBetweenTopics/__init__.py
@@ -2,9 +2,11 @@ import logging
 import json
 import azure.functions as func
 
+
 def main(subEvent,
-         pubEvent: func.Out[bytes]) -> None:
-    logging.info('Python function processed a TransferEventBetweenTopics request from the Dapr Runtime.')
+         pubEvent: func.Out[str]) -> None:
+    logging.info(
+        'Python function processed a TransferEventBetweenTopics request from the Dapr Runtime.')
     subEvent_json = json.loads(subEvent)
     payload = "Transfer from Topic A: " + str(subEvent_json["data"])
-    pubEvent.set(json.dumps({"payload": payload }))
+    pubEvent.set(json.dumps({"payload": payload}))
