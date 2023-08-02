a2c9adb248a44570cefb7c6747e95afc01249bed
MD Ashique <noorani.ashique5@gmail.com>
2023-08-02 15:20:03 +0530

    Update java-library/src/main/java/com/microsoft/azure/functions/dapr/annotation/DaprBindingOutput.java
    Co-authored-by: Shubham Sharma <shubhash@microsoft.com>

--- java-library/src/main/java/com/microsoft/azure/functions/dapr/annotation/DaprBindingOutput.java
+++ java-library/src/main/java/com/microsoft/azure/functions/dapr/annotation/DaprBindingOutput.java
@@ -20,7 +20,7 @@ import java.lang.annotation.ElementType;
 @CustomBinding(direction = "out", name = "", type = "daprBinding")
 public @interface DaprBindingOutput {
     /**
-     * The variable name used in function.json.
+     * Name of the dapr binding
      */
     String name();
 
